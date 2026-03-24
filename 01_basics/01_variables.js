27d355f32a92e4fe41714b8ce8d25fe39e762e78
somesh-pandey12 <someshpandeycil@gmail.com>
2026-03-24 02:32:46 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = "12421"
+let accountEmail = "somu72google@gmail.com"
+var accountPassword = "12321"
+accountCity = "Lucknow"
+
+accountEmail = "hch@gmail.com"
+accountPassword = "12321234"
+accountCity = "jaipur"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
