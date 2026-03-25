333b7e303fdc6bd32e1308c27bcbac14b6629d45
somesh-pandey12 <someshpandeycil@gmail.com>
2026-03-25 09:51:56 +0000

    compqrison

--- 01_basics/03_conversionoperator.js
+++ 01_basics/03_conversionoperator.js
@@ -8,3 +8,22 @@ console.log(booleanIsLoggedIn);
 // 1->true , 0 -> false
 // "" empty space -> false
 // "name" ->true
+
+
+// ********** OPERATIONS *********
+
+// console.log(2+3);
+// console.log(2-3);
+// console.log(2*3);
+// console.log(2/3);
+// console.log(2%3);
+// console.log(2**3);
+
+
+// console.log("1" + 2)   // 12
+// console.log(1 + "2")  //  12
+// console.log("1" + 2 + 2)  //  122
+// console.log(1 + 2 + "2")  //  32
+
+// console.log(+true) // 1
+// console.log(+"")  // 0 
