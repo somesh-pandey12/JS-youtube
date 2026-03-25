d7e1b498c51d0c3ac07be3755579d3d7198f9831
somesh-pandey12 <someshpandeycil@gmail.com>
2026-03-25 09:22:26 +0000

    Conversion of data types

---
+++ 01_basics/03_conversionoperator.js
@@ -0,0 +1,10 @@
+let score = "hitesh"
+let valueInNumber = Number(score)
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+// 1->true , 0 -> false
+// "" empty space -> false
+// "name" ->true
