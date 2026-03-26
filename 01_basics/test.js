4d7e5b4839404f320d0b290a3bdaefb0e82231e2
somesh-pandey12 <someshpandeycil@gmail.com>
2026-03-26 05:11:41 +0000

    strings

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,18 @@
-console.log("somu pandey")
+// console.log("somu pandey")
+
+// **********STRING **************
+const name = "som"
+const age = 21
+
+console.log(`hello my name is ${name} and my age is ${age}`);
+
+
+// const gameName = new String('suduko')
+// console.log(gameName[2]);
+// console.log(gameName.__proto__);
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// trim is used to remove spaces . ex "   som   "  = som
+// url.replace used to repacing
